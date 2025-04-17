3cef0b2637ed328acb192aedcef6f5aa7cb97d5b
kianGs4 <kian.ghassemi1383@gmail.com>
2025-04-18 01:24:10 +0330

    fix minor bug

--- scanner.py
+++ scanner.py
@@ -21,7 +21,7 @@ class Scanner:
             self.setOfLines = f.readlines()
      
     def get_next_token(self, input_program, location):
-        while location < len(input) and input_program[location] in WHITESPACE:
+        while location < len(input_program) and input_program[location] in WHITESPACE:
             location += 1
 
         if location > len(input_program): 
@@ -56,7 +56,7 @@ class Scanner:
             match = re.match(r'\d+[A-Za-z]*', input_program[location:])
             lexeme = match.group()
 
-            if re.fullmatch(lexeme, r'\d+'):
+            if re.fullmatch(r'\d+', lexeme):
                 return ('NUM', lexeme), location + len(lexeme)
             else:
                 self.errors.append((self.lineno, lexeme, 'Invalid number'))
@@ -70,12 +70,17 @@ class Scanner:
             return ('SYMBOL', ch), location + 1
         
          # Invalid Char
+        if ch == '\n': 
+            # self.lineno += 1
+            return None, location + 1
+        
         self.errors.append((self.lineno, ch, 'Invalid input'))
         return None, location + 1
     
     
         
     def scanning(self): 
+        tempLineno = self.lineno
         for _, line in enumerate(self.setOfLines): 
             self.lineno += 1
             lineTokenList = []
@@ -86,7 +91,7 @@ class Scanner:
                     lineTokenList.append(nextToken)
                 currentIndex = endToken
             if lineTokenList:
-                self.tokens.append(self.lineno, lineTokenList)
+                self.tokens.append((self.lineno, lineTokenList))
 
     def generateOutputs(self): 
         with open('token.txt', 'w', encoding='utf-8') as f: 
@@ -103,7 +108,7 @@ class Scanner:
 
 
         with open('symbol_table.txt', 'w', encoding='utf-8') as f:
-            for no, lexeme in enumerate(self.symbol_table, 1):
+            for no, lexeme in enumerate(self.symoblTable, 1):
                 f.write(f"{no}.\t{lexeme}\n")
 
 
