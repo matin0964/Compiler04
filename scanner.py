fa895c12f0e709d2993aaa9d8365d8f1ef26aa59
matin <matin.mathematics@gmail.com>
2025-04-17 18:29:10 +0330

    2

--- scanner.py
+++ scanner.py
@@ -13,6 +13,55 @@ def is_digit(c):
     if c >= '0' and c <= '9':
         return True
     return False
+def is_white(c):
+    match c:
+        case ' ':
+            return True
+        case '\t':
+            return True
+        case '\n':
+            return True
+        case '\r':
+            return True
+        case '\v':
+            return True
+        case '\f':
+            return True
+def is_symbol(c):
+    match c:
+        case ';':
+            return True
+        case ':':
+            return True 
+        case ',':
+            return True 
+        case '[':
+            return True 
+        case ']':
+            return True 
+        case '{':
+            return True 
+        case '}':
+            return True
+        case '(':
+            return True
+        case ')':
+            return True
+        case '+':
+            return True
+        case '-':
+            return True
+        case '*':
+            return True
+        case '/':
+            return True
+        case '=':
+            return True
+        case '<':
+            return True
+        case '>':
+            return True
+    return False
 def is_keyword(token):
     match token:
         case "if":
@@ -31,75 +80,40 @@ def is_keyword(token):
             return
 def get_next_token(input_program, location):
     # recognizing SYMBOL
-    match input_program[location]:
-        case ';':
+    ans= is_symbol(input_program[location])
+    if ans:
+        if input_program[location] == '/' and input_program[location + 1] == '*':
+            # comment 
             return
-        case ':':
+        if input_program[location] == '=' and input_program[location+1] == '=':
+            # logical equality
             return
-        case ',':
-            return
-        case '[':
-            return
-        case ']':
-            return
-        case '{':
-            return
-        case '}':
-            return
-        case '(':
-            return
-        case ')':
-            return
-        case '+':
-            return
-        case '-':
-            return
-        case '*':
-            return
-        case '/':
-            if input_program[location+1] == '*':
-                # comment
-                return
-            else:
-                return
-        case '=':
-            if input_program[location+1] == '=':
-                # equality
-                return
-            else:
-                # assignment
-                return
-        case '<':
-            return
-        case '>':
-            return
-        
-    # recognizing NUM
-    dummy_loc = location
-    tk = ''
-    is_Number = False
-    while is_digit(input_program[dummy_loc]):
-         is_Number = True
-         tk = tk + input_program[dummy_loc]
-         dummy_loc = dummy_loc+1
-    if is_Number:
-        return
-    
-    # recognizing ID
-    tk = ''
-    is_ID = False
-    dummy_loc = location
-    if is_digit(input_program[dummy_loc]):
-        # can't start an ID
-        return
-    elif is_letter(input_program[dummy_loc]):
-        tk = tk + input_program[dummy_loc]
-        dummy_loc = dummy_loc+1
-    else: 
+        # return the symbol token
         return 
-    while is_digit(input_program[dummy_loc]) or is_letter(input_program[dummy_loc]):
-        tk = tk + input_program[dummy_loc]
-        dummy_loc = dummy_loc+1
-    # recognize difference between ID and KEYWORD
-    
-    is_ID = True
+    # recognizing NUM
+    loc_dummy = location
+    tk = ''
+    if is_digit(input_program[location]):
+        while is_digit(input_program[loc_dummy]):
+            tk += input_program[loc_dummy]
+            loc_dummy += 1
+        if is_white(input_program[loc_dummy]) or is_symbol(input_program[loc_dummy]) or input_program[loc_dummy] == 'EOF':
+            # means that it is a correct number
+            return 
+        else:
+            # means that there's an error
+            return
+        return
+    elif is_letter(input_program[location]):
+        while is_letter(input_program[loc_dummy]) or is_digit(input_program[loc_dummy]):
+            tk += input_program[loc_dummy]
+            loc_dummy += 1
+        if is_keyword(tk):
+            # means that it is a keyword
+            return
+        else:
+            # the word is an ID and not a keyword
+            return
+        return 
+    else:
+        return
